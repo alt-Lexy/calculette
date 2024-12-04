26771cd701e4e708703ba7dd3b4d0c2382bfb08b
alexia <alexia.lievin@gmail.com>
2024-12-04 10:44:24 +0100

    feat(console): add test

---
+++ calculator/test/AdditionFunctionTests.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using calculator.Operations;
+
+namespace calculator.test
+{
+    public class AdditionFunctionTests
+    {
+        // Naming convension - ClassName_MethodeName_ExpectedResult
+        public void AdditionFunction_Addition_ReturnVoid()
+        {
+            Logger logger = new();
+            try
+            {
+                int firstNb = 2;
+                int secondNb = 10;
+
+                IOperation resultat = new Addition(firstNb, secondNb);
+
+                Calculator addition = new Calculator(resultat);
+
+                addition.Execute();
+
+                logger.LoggerFn($"ok => expected: 12 and actual: {addition.Resultat}");
+
+            }
+            catch (Exception e)
+            {
+                logger.LoggerFn($"{e}");
+                throw;
+            }
+
+        }
+
+    }
+}
